97b77174a475ff8176d3bfffa26c035e82c8b1e4
Akhil Gautam <akhilgautam123@gmail.com>
2020-12-14 23:37:02 +0530

    Added header and writer component

---
+++ src/FBClone/components/Writer.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const Writer = () => {
+  return (
+    <div className="flex items-center px-2 space-x-3">
+      <div className="overflow-hidden">
+        <img
+          alt=""
+          src="https://bit.ly/3oGOCc8"
+          className="h-12 w-12 object-cover rounded-full"
+        />
+      </div>
+      <textarea
+        className="h-20 w-3/5 rounded-lg border-2 pl-2 pt-2 focus:outline-none focus:border-gray-900"
+        placeholder="Write something here..."
+      />
+      <div className="w-12 h-12">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="#f2f2f2"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
+          />
+        </svg>
+      </div>
+    </div>
+  );
+};
+
+export default Writer;
--- src/FBClone/components/index.js
+++ src/FBClone/components/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
 import Stories from './Stories';
+import Header from './Header';
+import Writer from './Writer';
 
 const FBClone = () => {
   return (
-    <div className="flex flex-col overflow-x-hidden w-full">
+    <div className="flex flex-col overflow-x-hidden w-full space-y-2">
+      <Header />
       <Stories />
+      <Writer />
     </div>
   );
 };
